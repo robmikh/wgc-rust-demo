f79b65aca802906e99d6b9ed71fea6e8067a1b41
Robert Mikhayelyan <rob.mikh@outlook.com>
2020-05-08 00:52:28 -0700

    window finder

--- src/main.rs
+++ src/main.rs
@@ -13,37 +13,122 @@ mod d3d;
 mod encoder;
 mod roapi;
 mod snapshot;
+mod window_finder;
 
 use d3d::D3D11Device;
 use roapi::{ro_initialize, RoInitType};
 use snapshot::CaptureSnapshot;
-use winapi::um::winuser::{GetDesktopWindow, MonitorFromWindow, MONITOR_DEFAULTTOPRIMARY};
+use window_finder::find_window;
+use winapi::um::winuser::{GetDesktopWindow, MonitorFromWindow, MONITOR_DEFAULTTOPRIMARY, GetWindowThreadProcessId};
+use clap::{Arg, App, ArgMatches, value_t};
+use std::io::Write;
 
-fn run() -> winrt::Result<()> {
+use windows::graphics::capture::GraphicsCaptureItem;
+
+fn run(matches: &ArgMatches) -> winrt::Result<()> {
     ro_initialize(RoInitType::MultiThreaded)?;
 
-    println!("Getting the capture item...");
-    let monitor = unsafe { MonitorFromWindow(GetDesktopWindow(), MONITOR_DEFAULTTOPRIMARY) };
-    let item = capture::create_capture_item_for_monitor(monitor)?;
-
-    println!("Setting up d3d...");
+    let item = get_capture_item_from_matches(matches)?;
     let d3d_device = D3D11Device::new()?;
     let device = d3d_device.to_direct3d_device()?;
-
-    println!("Taking snapshot...");
     let surface = CaptureSnapshot::take(&device, &item)?;
-
-    println!("Saving file...");
     encoder::save_d3d_surface(&device, &surface, "screenshot.png")?;
 
     Ok(())
 }
 
 fn main() {
-    let result = run();
+    let matches = App::new("wgc-rust-demo")
+        .version("0.1.0")
+        .author("Robert Mikhayelyan <rob.mikh@outlook.com>")
+        .about("A demo that saves screenshots of windows or monitors using Windows.Graphics.Capture and Rust/WinRT.")
+        .arg(Arg::with_name("window")
+            .short("w")
+            .long("window")
+            .value_name("window title query")
+            .help("Capture a window who's title contains the provided input")
+            .conflicts_with_all(&["monitor", "primary"])
+            .takes_value(true))
+        .arg(Arg::with_name("monitor")
+            .short("m")
+            .long("monitor")
+            .value_name("monitor number")
+            .help("Capture a monitor")
+            .conflicts_with_all(&["window", "primary"])
+            .takes_value(true))
+        .arg(Arg::with_name("primary")
+            .short("p")
+            .long("primary")
+            .help("Capture the primary monitor (default if no params are specified)")
+            .conflicts_with_all(&["window", "monitor"])
+            .takes_value(false))
+        .get_matches();
+
+    let result = run(&matches);
 
     // We do this for nicer HRESULT printing when errors occur.
     if let Err(error) = result {
         error.code().unwrap();
     }
+}
+
+fn get_capture_item_from_matches(matches: &ArgMatches) -> winrt::Result<GraphicsCaptureItem> {
+    let item = if matches.is_present("window") {
+        let query = matches.value_of("window").unwrap();
+        let windows = find_window(query);
+        let window = {
+            if windows.len() == 0 {
+                println!("No window matching '{}' found!", query);
+                std::process::exit(1);
+            } else if windows.len() == 1 {
+                windows[0].clone()
+            } else {
+                println!("{} windows found matching '{}', please select one:", windows.len(), query);
+                println!("    Num       PID    Window Title");
+                for (i, window) in windows.iter().enumerate() {
+                    let mut pid = 0;
+                    unsafe { GetWindowThreadProcessId(window.handle, &mut pid) };
+                    println!("    {:>3}    {:>6}    {}", i, pid, window.title);
+                }
+                let index: usize;
+                loop {
+                    print!("Please make a selection (q to quit): ");
+                    std::io::stdout().flush().unwrap();
+                    let mut input = String::new();
+                    std::io::stdin().read_line(&mut input).unwrap();
+                    if input.to_lowercase().contains("q") {
+                        std::process::exit(0);
+                    }
+                    let input = input.trim();
+                    let selection: Option<usize> = match input.parse::<usize>() {
+                        Ok(selection) => {
+                            if selection < windows.len() {
+                                Some(selection)
+                            } else {
+                                None
+                            }
+                        },
+                        _ => None,
+                    };
+                    if let Some(selection) = selection {
+                        index = selection;
+                        break;
+                    } else {
+                        println!("Invalid input, '{}'!", input);
+                        continue;
+                    };
+                };
+                windows[index].clone()
+            }
+        };
+        capture::create_capture_item_for_window(window.handle)?
+    } else if matches.is_present("monitor") {
+        let _id = value_t!(matches, "monitor", u32).unwrap();
+        unimplemented!("Specific monitor capture not implemented!");
+    } else {
+        let monitor = unsafe { MonitorFromWindow(GetDesktopWindow(), MONITOR_DEFAULTTOPRIMARY) };
+        capture::create_capture_item_for_monitor(monitor)?
+    };
+
+    Ok(item)
 }
